70859844577040cd3ff6d891d5638a05db2608ca
root <root@DESKTOP-11AU14A.lan>
2022-10-25 10:05:36 -0400

    Fixed the index.php

--- index.php
+++ index.php
@@ -13,5 +13,4 @@
  */
 define( 'WP_USE_THEMES', true );
 /** Loads the WordPress Environment and Template */
-//require __DIR__ . '/wp-blog-header.php';
-require once '../file.php'
+require __DIR__ . '/wp-blog-header.php';
